2ef9dc86665d096179c341634b2f8e88d4e8de28
kyle <rubberchickn@gmail.com>
2017-07-30 22:04:21 -0600

    need to also aggregate by year, otherwise getting duplicates

--- subscriber_report/subcriber_report.py
+++ subscriber_report/subcriber_report.py
@@ -5,13 +5,14 @@ import calendar
 
 class subscriber_report(object):
 
-    def __init__(self, filepath, agg_type, year_filter=None):
+    def __init__(self, filepath, agg_type, market=None, year_filter=None):
 
         self.filepath = filepath
         self.agg_type = agg_type
         self.year_filter = year_filter
+        self.market = market
 
-    def read_data(self, filepath):
+    def read_data(self, filepath, agg_type):
 
         data = pd.read_excel(filepath)
         data['date'] = data.activity_date.apply(lambda x: x.date())
@@ -25,9 +26,10 @@ class subscriber_report(object):
         data['quarter'] = data.month.apply(lambda x: (x-1)  // 3 + 1)
         data['is_last_day_of_quarter'] = np.where(((data.month == 3) & (data.dom == 31)) | ((data.month == 6) & (data.dom == 30)) |
                       ((data.month == 9) & (data.dom == 30)) | ((data.month == 12) & (data.dom == 31)), 1, 0)
+        #data['aggyear'] = data.year.apply(lambda x: agg_type+str(x))
         return data
 
-    def get_ending_subscribers(self, data, agg_type):
+    def get_ending_subscribers(self, data, agg_type, market):
 
         if agg_type == 'week':
             df = data[data.day == 7]
@@ -36,17 +38,24 @@ class subscriber_report(object):
         elif agg_type == 'quarter':
             df = data[data.is_last_day_of_quarter == 1]
 
-        total_subscribers = df[['total_subscribers', agg_type]]
+        if market:
+            total_subscribers = df[df.market == market][['total_subscribers', agg_type, 'year']]
+        else:
+            total_subscribers = df.groupby([agg_type, 'year']).agg({'total_subscribers':sum}).reset_index()
+
         return total_subscribers
 
-    def build_agg_data(self, data, total_subscribers, agg_type, year_filter=None):
+    def build_agg_data(self, data, total_subscribers, agg_type, market=None, year_filter=None):
 
         if year_filter:
             df = data[data.year == year_filter]
         else:
             df = data
 
-        agg_data = df.groupby(agg_type).agg(
+        if market:
+            df = df[df.market == market]
+
+        agg_data = df.groupby([agg_type, 'year']).agg(
             {'new_subscriptions': sum,
              'self_install': sum,
              'professional_install': sum,
@@ -56,10 +65,10 @@ class subscriber_report(object):
 
         agg_data['net_gain'] = agg_data.new_subscriptions - agg_data.total_disconnects
 
-        merged_agg_data = agg_data.reset_index().merge(total_subscribers, on = agg_type)
+        merged_agg_data = agg_data.reset_index().merge(total_subscribers, on=[agg_type, 'year'])
         merged_agg_data['beginning_subs'] = merged_agg_data.total_subscribers.shift(1)
 
-        end_df = merged_agg_data.transpose().reindex(['beginning_subs', 'new_subscriptions', 'self_install', 'professional_install',\
+        end_df = merged_agg_data.set_index(['year', agg_type]).sort_index().transpose().reindex(['beginning_subs', 'new_subscriptions', 'self_install', 'professional_install',\
                           'total_disconnects', 'post_install_returns', 'disconnects',\
                           'net_gain', 'total_subscribers']).rename(index={
                                                                 'beginning_subs':'Beginning Subscribers',
@@ -71,7 +80,7 @@ class subscriber_report(object):
                                                                 'disconnects':'Disconnects',
                                                                 'net_gain':'Net Gain',
                                                                 'total_subscribers':'Ending Subs'
-                          }).rename(columns=lambda x: agg_type+' '+str(x))
+                          })
 
         return end_df
 
@@ -79,9 +88,10 @@ class subscriber_report(object):
         filepath = self.filepath
         agg_type = self.agg_type
         year_filter = self.year_filter
+        market = self.market
 
-        data = self.read_data(filepath)
-        total_subscribers = self.get_ending_subscribers(data, agg_type)
+        data = self.read_data(filepath, agg_type)
+        total_subscribers = self.get_ending_subscribers(data, agg_type, market)
         report = self.build_agg_data(data, total_subscribers, agg_type, year_filter)
 
         return report
